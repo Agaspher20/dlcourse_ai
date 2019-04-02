6a4eaa29bdc5c26fb15c416400fc5e4ea4af53c7
Michael Naumenko <agaspher@outlook.com>
2019-04-02 09:36:07 +0300

    Grid search on trainer first

--- dlcourse_ai/assignments/assignment2/modelGridSearch.py
+++ dlcourse_ai/assignments/assignment2/modelGridSearch.py
@@ -18,9 +18,33 @@ def search_model(
 
     initial_values = trainer.fit()
 
-    best_model_keys = {}
     best_loss_history, best_train_history, best_val_history = initial_values
-    best_train = np.max(best_train_history)
+    best_train = np.mean(best_train_history)
+    best_trainer_keys = {}
+    best_trainer = trainer
+
+    for key in trainer_grid:
+        best_trainer_keys[key] = getattr(best_trainer, key)
+
+        for value in trainer_grid[key]:
+            model = model_factory()
+
+            trainer = initialize_model(lambda: trainer_factory(model), best_trainer_keys)
+            setattr(trainer, key, value)
+        
+            # You should expect loss to go down and train and val accuracy go up for every epoch
+            loss_history, train_history, val_history = trainer.fit()
+            mean_train = np.mean(train_history)
+        
+            if mean_train > best_train:
+                best_train = mean_train
+                best_trainer_keys[key] = value
+                best_loss_history = loss_history
+                best_train_history = train_history
+                best_val_history = val_history
+        print("\nBest {} is {} with accuracy {}\n".format(key, best_trainer_keys[key], best_train))
+
+    best_model_keys = {}
     best_model = model
 
     for key in model_grid:
@@ -34,40 +58,16 @@ def search_model(
         
             # You should expect loss to go down and train and val accuracy go up for every epoch
             loss_history, train_history, val_history = trainer.fit()
-            max_train = np.max(train_history)
+            mean_train = np.mean(train_history)
         
-            if max_train > best_train:
-                best_train = max_train
+            if mean_train > best_train:
+                best_train = mean_train
                 best_model_keys[key] = value
-                best_loss_history = loss_history,
-                best_train_history = train_history,
+                best_loss_history = loss_history
+                best_train_history = train_history
                 best_val_history = val_history
                 best_model = model
         print("\nBest {} is {} with accuracy {}\n".format(key, best_model_keys[key], best_train))
-
-    best_trainer_keys = {}
-    best_trainer = trainer_factory(model_factory())
-
-    for key in trainer_grid:
-        best_trainer_keys[key] = getattr(best_trainer, key)
-
-        for value in trainer_grid[key]:
-            model = initialize_model(model_factory, best_model_keys)
-
-            trainer = initialize_model(lambda: trainer_factory(model), best_trainer_keys)
-            setattr(trainer, key, value)
-        
-            # You should expect loss to go down and train and val accuracy go up for every epoch
-            loss_history, train_history, val_history = trainer.fit()
-            max_train = np.max(train_history)
-        
-            if max_train > best_train:
-                best_train = max_train
-                best_trainer_keys[key] = value
-                best_loss_history = loss_history,
-                best_train_history = train_history,
-                best_val_history = val_history
-        print("\nBest {} is {} with accuracy {}\n".format(key, best_trainer_keys[key], best_train))
 
     return (
         best_model,
